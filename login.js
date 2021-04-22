068925e6a3371c2290f7e16c67dc900d27d84187
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-04-22 22:26:51 +0530

    email and name added, fruit add or removed added

---
+++ login.js
@@ -0,0 +1,11 @@
+const submitDetails = () => {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  if (name && email) {
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    window.location.href = "home.html";
+  } else {
+    alert("Please fill all the details");
+  }
+};
