3d8895634879eb52e43bb88481911ffd5c2f432d
Piyush Hinduja <piyushinduja47@gmail.com>
2021-04-22 22:51:59 +0530

    CSS changes made

--- cart.js
+++ cart.js
@@ -26,16 +26,15 @@ const loadCart = () => {
             <img alt="" height="200" src="${c.name}.jpg" width="200" />
         </td>
         <td class="auto-style3" style="width: 318px">
-            <span class="auto-style7">${c.name}</span><br class="auto-style5" />
-        <td class="auto-style8" style="width: 300px">
-        <td>${c.count}</td>
-        <td>${c.totalPrice}</td>
+            <span class="auto-style7" style="font-size:20px">${c.name}</span><br class="auto-style5" />
+        <td class='auto-style5' style="font-size:20px">${c.count}kg</td>
+        <td class='auto-style5' style="font-size:20px">Rs. ${c.totalPrice}</td>
         <td class="auto-style2">
             <button class="button1" onclick=removeItem('${c.name}')>Remove</button>
         </td>
         </tr>`
     })
-    html += `<p class= ''>Total Price: ${total}</p>
+    html += `<p class= 'auto-style12'>Total Price: Rs.${total}</p>
 `
     document.getElementById('cartTable').innerHTML = html
 }
