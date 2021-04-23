06456941a9e162ed4bc55c8fc3912336806aa4a7
Piyush Hinduja <piyushinduja47@gmail.com>
2021-04-23 16:15:28 +0530

    CSS changes

--- cart.js
+++ cart.js
@@ -27,17 +27,19 @@ const loadCart = () => {
             <img alt="" height="200" src="${c.name}.jpg" width="200" />
             </td>
             <td class="auto-style3" style="width: 318px">
-            <span class="auto-style7">${c.name}</span><br class="auto-style5" />
-            <td class="auto-style8" style="width: 300px">
-            <td>${c.count}</td>
-            <td>${c.totalPrice}</td>
+            <span class="auto-style11">${c.name}</span><br class="auto-style5" />
+
+            <td class="auto-style11">${c.count} kg</td>
+            <td class="auto-style11">Rs.${c.totalPrice}</td>
             <td class="auto-style2">
             <button class="button1" onclick=removeItem('${c.name}')>Remove</button>
             </td>
             </tr>`;
     }
   });
-  html += `<p class= ''>Total Price: ${total}</p>
-`;
+  html += `<p class="auto-style12">Total Price: ${total}</p>`;
   document.getElementById("cartTable").innerHTML = html;
 };
+const placed_order_message = () =>{
+  window.alert("Order placed Successfully. Thank You for shopping with DailyFresh.com")
+}
